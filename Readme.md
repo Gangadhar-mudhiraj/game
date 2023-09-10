70b52ce48f6520916e75a5b348d9b1d81a18142d
Gangadhar <2211it010097@mallareddyuniversity.ac.in>
2023-09-10 23:18:36 +0530

    Added all requird files

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# this project is done using simple html css and javascript
+
+<h1>Brightness controller done with DOM events funtions and events listner functions in java script</h1>
---
+++ app.js
@@ -0,0 +1,73 @@
+let gmse = [];
+let user = [];
+let btns = ["o", "tw", "th", "f"];
+let started = false;
+let level = 0;
+let h2 = document.querySelector("h2");
+let btn = document.querySelector(".box");
+document.addEventListener("keypress", function () {
+  if (started == false) {
+    started = true;
+    levelup();
+  }
+});
+
+function usfl(btn) {
+  btn.classList.add("usfl");
+  setTimeout(function () {
+    btn.classList.remove("usfl");
+  }, 250);
+}
+function btnfl(btn) {
+  btn.classList.add("flash");
+  // console.log(user);
+
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  }, 250);
+}
+function levelup() {
+  user = [];
+  level++;
+  h2.innerText = `level ${level}`;
+  let a = Math.floor(Math.random() * 3);
+  let randcol = btns[a];
+  let ranbtn = document.querySelector(`.${randcol}`);
+  gmse.push(randcol);
+  btnfl(ranbtn);
+  console.log(gmse);
+}
+
+function chans(ind) {
+  if (user[ind] == gmse[ind]) {
+    if (user.length == gmse.length) {
+      setTimeout(levelup, 1000);
+    }
+  } else {
+    h2.innerText = `Game over ! your score is ${level} press any ket to start `;
+    document.querySelector("body").style.backgroundColor = "red";
+    setTimeout(function () {
+      document.querySelector("body").style.backgroundColor = "gray";
+    }, 1000);
+    reset();
+  }
+}
+
+function btnpre() {
+  usfl(this);
+  uscl = this.getAttribute("id");
+  user.push(uscl);
+  chans(user.length - 1);
+}
+
+let allb = document.querySelectorAll(".box");
+for (x of allb) {
+  x.addEventListener("click", btnpre);
+}
+
+function reset() {
+  started = false;
+  gmse = [];
+  level = 0;
+  user = [];
+}
